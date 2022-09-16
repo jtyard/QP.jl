1de9589b773e38ae46bc58f75fcc30d758dd015f
Jon Yard <jyard@uwaterloo.ca>
2022-09-16 14:53:07 -0400

    QUAF

--- Magma/fib.m
+++ Magma/fib.m
@@ -1,15 +1,17 @@
 
+
+
+RealCyclotomicField := function(n)
+    F := CyclotomicField(n);
+    return sub<F|F.1 + F.1^-1>;
+end function;
+
 Dn := function(n)
     if (n mod 4) eq 0 then 
         return 1;
     else
         return 4 - (RealCyclotomicField(n).1)^2;
     end if;
-end function;
-
-RealCyclotomicField := function(n)
-    F := CyclotomicField(n);
-    return sub<F|F.1 + F.1^-1>;
 end function;
 
 su2levelk := function(k) 
@@ -21,3 +23,7 @@ end function;
 A := su2levelk(3);
 
 OA := MaximalOrder(A);
+
+allinvs := function(OO)    
+    return [#TwoSidedIdealClassGroup(OO),#ConjugacyClasses(OO), #LeftIdealClasses(OO),#RightIdealClasses(OO)];
+end function;
