18e7e98aed360ed444a8a8533b682b0abe62d5d8
Jon Yard <jyard@uwaterloo.ca>
2023-06-14 09:09:53 -0400

    minor

--- sic/sic2.jl
+++ sic/sic2.jl
@@ -18,9 +18,9 @@ F = cyclotomic_field(12)[1]
 
 rational_solutions(change_base_ring(F,I)) 
 
-X = heis(0,1,2)
-Y = heis(1,1,2)
-Z = heis(1,0,2)
+# X = heis(0,1,2)
+# Y = heis(1,1,2)
+# Z = heis(1,0,2)
 
 
 Phi = (identity_matrix(F,2) + map(F,heis(0,1,2) + heis(1,0,2) + heis(1,1,2))/sqrt(F(3)))/2
