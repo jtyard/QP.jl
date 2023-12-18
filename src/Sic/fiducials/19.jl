839bfdec4c7591f88cd000426e439daf2e0022dc
Jon Yard <jyard@uwaterloo.ca>
2023-12-17 21:10:36 -0500

    towards ring class field

--- src/Sic/fiducials/19.jl
+++ src/Sic/fiducials/19.jl
@@ -1,2 +1,27 @@
-# Build the dimension 19 fields
+# Build the 19abcd fields
+#
+# D = (19-3)(19+1) = 320 
+# D0 = 5
+# f = 8
+# a - adjoin conductor 8 ring class field
+# b - adjoin conductor 4 ring class field
+# c - adjoin conductor 2 ring class field
+# d is the ray class field
+#
 S = SicData(19)
+K = S.K 
+OK = S.OK 
+
+rcf = ray_class_field(64*S.OK,S.inf)
+
+rcg, ph = ray_class_group(64*S.OK,S.inf)
+
+art = artin_map(rcf)
+
+PZ = [ph(g) for g in rcg if is_principal_fac_elem(ph(g))[1]]
+length(PZ)
+
+a = PZ[2]
+
+art.map1(a)
+# how to put a in rcf???
