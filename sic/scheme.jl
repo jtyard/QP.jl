677eed784463f4860d8c6dbd38e27c8305fc1a4b
Jon Yard <jyard@uwaterloo.ca>
2023-12-31 22:19:49 -0500

    computing alldim

--- sic/scheme.jl
+++ sic/scheme.jl
@@ -1,11 +1,22 @@
 using Oscar, QP
 
-N = 2
+N = 3
 
-S = projective_matrix_space(QQ,N)
+P = projective_matrix_space(QQ,N)
 
-X = gen(S)
+X = gen(P)
 
-laplacian(S,hplus(S,1,1))
+laplacian(P,hplus(S,1,1))
 
 
+Treal = subscheme(P.P,Im(P) + Ihplus(P) + Ireal(P))
+T1 = subscheme(P.P,Im(P) + Ihplus(P))
+
+Tall = subscheme(P.P,Ihminus(S) + Ihplus(P))
+
+# dim(Tall)=1,5,7,13 for N=2,3,4,5 but N=6 takes a long time. 
+
+function alldim(N)
+    P = projective_matrix_space(QQ,N)
+    dim(subscheme(P.P,Ihminus(P) + Ihplus(P)))
+end
