195bdb54586c8ea1fbdea1d9080805e6ac7aa403
Jon Yard <jyard@uwaterloo.ca>
2024-01-01 18:18:54 -0500

    initial

---
+++ sic/scheme3.jl
@@ -0,0 +1,18 @@
+using Oscar, QP
+
+N = 3
+
+P = projective_matrix_space(QQ,N)
+
+I = Im(P) + Ihplus(P) #+ Ireal(P)
+
+
+# Actually I think there are no real solutions but maybe a different symmetry is needed
+
+#T = subscheme(P.P,I)
+#Tall = subscheme(P.P,Ihminus(S) + Ihplus(P))
+
+#rational_solutions(I)
+
+
+#@time pd = primary_decomposition(I)
