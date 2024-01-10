63ab4f195b7da8b1f35e51348df45f54add3fd03
Jon Yard <jyard@uwaterloo.ca>
2024-01-10 18:16:02 -0500

    quadratic designs, real versions

--- sic/scheme2.jl
+++ sic/scheme2.jl
@@ -6,7 +6,7 @@ C12 = cyclotomic_field(12)[1]
 
 P12 = projective_matrix_space(C12,N)
 
-I12 = Im(P) + Ihplus(P)
+I12 = Im(P12) + Ihplus(P12)
 
 #T = subscheme(P.P,I)
 #Tall = subscheme(P.P,Ihminus(S) + Ihplus(P))
@@ -16,6 +16,13 @@ I12 = Im(P) + Ihplus(P)
 P = projective_matrix_space(QQ,N)
 
 I = Im(P) + Ihplus(P)
+
+Ir = Ireal(P) + I
+
+Iall = Ihplus(P) + Ihminus(P)
+
+Iallr = Ihplus(P) + Ihminus(P) + Ireal(P)
+
 
 #T = subscheme(P.P,I)
 #Tall = subscheme(P.P,Ihminus(S) + Ihplus(P))
