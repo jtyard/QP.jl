c0b03614d9010375ddf7e1ad20b043ce5107876d
Jon Yard <jyard@uwaterloo.ca>
2024-01-10 18:19:47 -0500

    attempts to cut out primes seem not to work

--- sic/scheme7.jl
+++ sic/scheme7.jl
@@ -3,12 +3,12 @@ using Oscar, QP
 N = 7
 
 S = SicData(7)
-F = S.F
 
-P = projective_matrix_space(QQ,N)
+P = S.P
 
-I = Im(P) + Ihplus(P) + Ireal(P) + It(P,Z7[2 0; 0 4])
-
+I = Im(P) + Ihplus(P)  + It(P,Z7[2 0; 0 4])
+J = Im(P) + Ihplus(P)  + It(P,Z7[3 0; 0 5])
+Ir = Im(P) + Ihplus(P) + Ireal(P) + It(P,Z7[2 0; 0 4])
 # Actually I think there are no real solutions but maybe a different symmetry is needed
 
 T = subscheme(P.P,I)
@@ -16,4 +16,4 @@ T = subscheme(P.P,I)
 
 #rational_solutions(I)
 
-mp = minimal_primes(I)
+#mp = minimal_primes(I)d
