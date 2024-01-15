76bca2abdbe92bb786ffcbbcf6b8e9226377fa87
Jon Yard <jyard@uwaterloo.ca>
2024-01-14 20:35:22 -0500

    moved

--- clif/originalT.jl
+++ clif/originalT.jl
@@ -9,7 +9,7 @@ mat(v) = matrix_space(C12,2,2)([v[1] + v[2]*z3, v[3] + v[4]*z3, v[5] + v[6]*z3, 
 
 box(N) = Base.Iterators.product(repeat([-N:N],8)...)
 
-I = matrix_space(F,2,2)([1,0,0,1])
+I = matrix_space(C12,2,2)([1,0,0,1])
 
 #U2 = [mat(v) for v in box(2) if mat(v)*dagger(mat(v))==9*I]
 #SU2 = [g for g in U2 if det(g) == 1]
@@ -21,7 +21,7 @@ I = matrix_space(F,2,2)([1,0,0,1])
 t12 = C12[3 1-i; 1+i -3]
 
 x = C12[0 1; 1 0]
-y = C12[0 -i;i 1]
+y = C12[0 -i;i 0]
 z = C12[1 0; 0 -1]
 
-u = (1 + i*(x+y+z))
+u = (1 + i*(x+y+z))*C12(2)^-1
