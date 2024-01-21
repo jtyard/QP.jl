d917659eb81777028a8db09a742b91848164825b
Jon Yard <jyard@uwaterloo.ca>
2024-01-20 21:11:23 -0500

    minor

--- clif/counts.jl
+++ clif/counts.jl
@@ -2,17 +2,25 @@ using Oscar, QP
 
 
 F,i = cyclotomic_field(4)
+
 mat(v) = matrix_space(F,2,2)([v[1] + v[2]*i, v[3] + v[4]*i, v[5] + v[6]*i,  v[7] + v[8]*i])
 
 box(N) = Base.Iterators.product(repeat([-N:N],8)...)
 
 I = matrix_space(F,2,2)([1,0,0,1])
 
-U2 = [mat(v) for v in box(2) if mat(v)*dagger(mat(v))==4*I]
-SU2 = [g for g in U2 if det(g) == 4]
+U2 = [mat(v) for v in box(1) if mat(v)*dagger(mat(v))==I]
 
-length(U2), length(SU2)
-#96, 24 so 
+SU2 = [g for g in U2 if det(g) == 1]
+
+O2 = [u for u in U2 if dagger(u) == transpose(u)]
+
+SO2 = [u for u in O2 if det(u)==1]
+
+
+length(U2), length(SU2), length(O2), length(SO2)
+#(32, 8, 8, 4)
+#(96, 24, 8, 4)
 
 
 #length([mat(v) for v in box(2) if mat(v)*transpose(mat(v))==4*I])
