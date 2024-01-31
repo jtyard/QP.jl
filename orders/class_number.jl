1489dce2a0606318d11705ef2b4d368eb7631a16
Jon Yard <jyard@uwaterloo.ca>
2024-01-31 18:40:34 -0500

    computes class number of indefinite quaternion algebras

---
+++ orders/class_number.jl
@@ -0,0 +1,45 @@
+# Class number and related methods for quaternion orders
+
+using Oscar, QP  
+
+
+K,s = quadratic_field(2)
+A = quaternion_algebra(QQ,-1,-1)
+B = quaternion_algebra(K,1,1+s)
+
+OA = maximal_order(A)
+OB = maximal_order(B)
+
+
+Ord = Hecke.AlgAssRelOrd # could do Union{Hecke.AlgAssAbsOrd,Hecke.AlgAssRelOrd} but disc not an ideal
+
+
+
+class_number(A::Hecke.AlgQuat) = class_number(maximal_order(A))
+split_real_places(A::Hecke.AlgQuat) = split_real_places(maximal_order(A))
+
+
+# Following Section 5 of https://arxiv.org/abs/0808.3833
+function class_number_totally_definite(O::Ord)
+    D = discriminant(O)
+    facD = factor(D)
+end
+
+function split_real_places(A::Hecke.AlgQuat)
+    K = base_ring(A)
+    a,b = A.std
+    return [v for v in real_places(K) if is_negative(a,v) | is_negative(b,v)]
+end
+
+function class_number(O::Ord)
+    A = algebra(O)
+    @assert A isa Hecke.AlgQuat
+    K = base_ring(A)
+    srp = split_real_places(A)
+    if length(srp) == degree(K)
+        return class_number_totally_definite(O)
+    else
+        return order(ray_class_group(1*maximal_order(K),srp)[1])
+    end
+end
+
