4391834d281cee79e12e8b30204fbe277547c965
Jon Yard <jyard@uwaterloo.ca>
2024-01-31 18:39:35 -0500

    examples

--- clif/auts.jl
+++ clif/auts.jl
@@ -8,8 +8,8 @@ L = hermitian_lattice(F; gram = identity_matrix(F,2))
 
 K,s = quadratic_field(2)
 OK = maximal_order(K)
+M = quadratic_lattice(K; gram = identity_matrix(K,2)) 
 
-M  = lattice(OK;gram = matrix(OK,2,2,[2,s,s,2]))
 
 #order(automorphism_group(L))
 # 32
@@ -23,3 +23,6 @@ M  = lattice(OK;gram = matrix(OK,2,2,[2,s,s,2]))
 
 #order(automorphism_group(hermitian_lattice(F; gram = identity_matrix(F,4))))
 #6144
+
+
+#M  = lattice(OK;gram = matrix(OK,2,2,[2,s,s,2])) # doesn't work
