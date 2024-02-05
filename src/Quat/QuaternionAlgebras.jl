beb8b0aae2b380b9bc8266174dfe63985ec2111f
Jon Yard <jyard@uwaterloo.ca>
2024-02-04 20:01:00 -0500

    fixed

--- src/Quat/QuaternionAlgebras.jl
+++ src/Quat/QuaternionAlgebras.jl
@@ -4,4 +4,4 @@ using QP
 export quaternion_algebra
 
 # A wrapper that coerces the generators automatically - should merge into OSCAR
-quaternion_algebra(K,a,b) = QuaternionAlgebra(K,K(a),K(b))
+quaternion_algebra(K,a,b) = Hecke.QuaternionAlgebra(K,K(a),K(b))
