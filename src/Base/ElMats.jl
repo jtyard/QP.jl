d1e9d8d13c4ad1732f374080a9ab02f2cdc720e9
Jon Yard <jyard@uwaterloo.ca>
2024-04-01 17:02:08 -0400

    Partial trace

--- src/Base/ElMats.jl
+++ src/Base/ElMats.jl
@@ -2,9 +2,10 @@
 # Elementary matrices
 #################
 
-import Oscar.Hecke.⊗ as hecketensor
+#import Oscar.Hecke.⊗ as hecketensor
 
-export Eij, SWAP, Pplus, Pminus, werner, bra, ket, tensor, ⊗, tensorperm
+export Eij, SWAP, Pplus, Pminus, werner, bra, ket, tensor, tensorperm #, ⊗
+export partial_trace
 
 # Matrix units
 Eij(i::Int,j::Int,N::Int) = matrix(ZZ,[[a == (i % N) && b == (j % N) for b in 0:N-1] for a in 0:N-1])
@@ -30,7 +31,6 @@ ket(a::FinGenAbGroupElem) = tensor([ket(Int(a[i]),Int(order(gens(parent(a))[i]))
 
 bra(a...) = transpose(ket(a...))
 
-
 # Tensor products
 
 #tensor = kronecker_product
@@ -45,7 +45,7 @@ function Oscar.tensor(A...)
 end
 
 #import Hecke.⊗
-⊗=tensor
+#Hecke.⊗=tensor
 
 #A::MatElem ⊗ B::MatElem = tensor(A,B)
 # Conflicts with Hecke.⊗ maybe import it directly?
@@ -63,11 +63,25 @@ function tensorperm(d,p)
     U
 end
 
-
-
 # Useful matrices on the tensor product
 
 SWAP(N) = sum([kronecker_product(Eij(i,j,N),Eij(j,i,N)) for i in 1:N for j in 1:N])
 Pplus(N) = (1//2)*(identity_matrix(QQ,N^2) + SWAP(N))
 Pminus(N) = (1//2)*(identity_matrix(QQ,N^2) - SWAP(N))
 werner(N,a) = (identity_matrix(QQ,N^2) + a*map(QQ,SWAP(N)))/(N^2 + a*N)
+
+
+function partial_trace(A::MatElem, m,n)
+    r, c = size(A)
+    @assert r==c==m*n
+    B = zero_matrix(base_ring(A),m,m)
+    for i = 0:m-1
+        for j = 0:m-1
+            B[i+1,j+1] = tr(A[m*i+1:m*i+n,m*j+1:m*j+n])
+        end
+    end
+    B
+end
+
+    
+    
