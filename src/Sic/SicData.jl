4ea3b380963d59942781c052451ceb25d80c1513
Jon Yard <jyard@uwaterloo.ca>
2024-04-13 22:48:03 -0400

    adding ideals is slow

--- src/Sic/SicData.jl
+++ src/Sic/SicData.jl
@@ -6,8 +6,6 @@ mutable struct SicData
     D0::ZZRingElem
     f::ZZRingElem
     P::ProjectiveMatrixSpace
-    I::MPolyIdeal
-    Iall::MPolyIdeal
     K::NumField
     inf::Vector{InfPlc}
     OK::NumFieldOrder # the maximal order Z[uf]
@@ -19,37 +17,48 @@ mutable struct SicData
     F::NumField
     c::NumFieldHom
     e::Hecke.NumFieldEmb # or HeckeNumFieldEmbNfNS ?
+    I::MPolyIdeal
+    Iall::MPolyIdeal
     #ring_class_field::ClassField
     #function SicData(d::Int;build_nf=true)
     @memoize function SicData(d::Int;build_nf=true)
         if d == 2
             P = projective_matrix_space(QQ,d)
-            I = Ihplus(P) + Iminors(P,2)
-            Iall = Ihplus(P) + Ihminus(P)
-            return new(2,-3,-3,1,P,I,Iall,rationals_as_number_field()[1])
+            S = new(2,-3,-3,1,P,rationals_as_number_field()[1])
+            S. I = Ihplus(P) + Iminors(P,2)
+            S. Iall = Ihplus(P) + Ihminus(P)
+            return S
         end
         if d == 3
             P = projective_matrix_space(QQ,d)
-            I = Ihplus(P) + Iminors(P,2)
-            Iall = Ihplus(P) + Ihminus(P)
-            return new(3,0,0,1,P,I,Iall,rationals_as_number_field()[1])
+            S = new(3,0,0,1,P,rationals_as_number_field()[1])
+            S.I = Ihplus(P) + Iminors(P,2)
+            S.Iall = Ihplus(P) + Ihminus(P)
+            return S
         end
         D = ZZ((d-3)*(d+1))
         D0 = fundamental_discriminant(D) 
         f = ZZ(sqrt(D//D0)) 
+        #println("making projective space")
         P = projective_matrix_space(QQ,d)
-        I = Ihplus(P) + Iminors(P,2)
-        Iall = Ihplus(P) + Ihminus(P)
+        #println("Ihplus + Iminors")
+        #I = Ihplus(P) + Iminors(P,2)
+        #println("Ihplus + Ihminus")
+        #Iall = Ihplus(P) + Ihminus(P)
         K = quadratic_field(Hecke.squarefree_part(D0))[1] 
+        #println("finding real places")
         inf = real_places(K)
+        #println("computing maximal order")
         OK = maximal_order(K)
+        #println("finding fundamental unit")
         uf = d > 3 ? fundamental_unit(OK) : OK(1)
         OD = quadratic_order(D)
         bb = (d-1 + sqrt(K(D)))//K(2)
         Ob = quadratic_order(bb)
         b = Ob(bb)
+        #println("creating rcf")
         rcf = ray_class_field((isodd(d) ? d : 2*d)*OK,infinite_places(K))
-        
+        #println("created rcf")
         #println("Constructing new")
         S = new(
         d,
@@ -57,8 +66,6 @@ mutable struct SicData
         D0,
         f,
         P,
-        I,
-        Iall,
         K,
         inf,
         OK,
@@ -68,11 +75,12 @@ mutable struct SicData
         b,
         rcf)
         if build_nf
-            #println("Constructing number field")
+            println("Constructing number field")
             S.F = number_field(rcf)
             #F = S.F
             #S.F = number_field(rcf,using_stark_units = true)
             #println("Finding complex conjugation")
+            println("Finding complex conjugation")
             S.c = complex_conjugation(rcf,inf[2])
             
             # would be nice if we could do
