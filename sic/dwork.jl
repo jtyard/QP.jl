99895f667be271d17b47b16e1779060db8faf115
Jon Yard <jyard@uwaterloo.ca>
2024-04-15 16:17:16 -0400

    initial

---
+++ sic/dwork.jl
@@ -0,0 +1,24 @@
+using Oscar, QP
+
+S = SicData(5)
+
+Psi = fiducial(5)
+
+F = S.F 
+
+rcf = S.rcf
+
+mu = dwork_modulus(Psi)
+
+art = artin_map(rcf)
+
+art1 = art.map1
+art2 = art.map2
+
+gal = domain(art2)
+
+stab = sub(gal,[g for g in gal if art2(g)(mu) == mu])[1]
+
+ng, m = norm_group(rcf)
+
+hcf = fixed_field(rcf,sub(ng,[g for g in ng if art(m(g))(mu) == mu])[1])
