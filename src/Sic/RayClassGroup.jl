1146a428855433457023e401845136bf06b3eb77
Jon Yard <jtyard@gmail.com>
2025-01-09 19:23:14 -0500

    removed

--- src/Sic/RayClassGroup.jl
+++
@@ -1,9 +0,0 @@
-# It might be nice to have an explicit type for ray class groups that e.g. ideals can be coerced into.  I didn't get far with this though.
-
-using Oscar
-
-mutable struct RayClassGroup
-    C  # The group
-    ph # C -> ideals 
-end
-
